a65c8367d3d3fdc4546557248f292a845ee26aea
Louis-Olivier Major <louis.olivier.major@gmail.com>
2023-11-21 10:49:12 -0500

    Initial commit

---
+++ TravelFeeCalculator.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class TravelFeeCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(TravelFeeCalculator))
+        label_title = New Label()
+        label_number_of_kilometers = New Label()
+        textBox_num_of_km = New TextBox()
+        label_gas_consumption = New Label()
+        textBox_gas_consumption = New TextBox()
+        label_gas_price = New Label()
+        textBox_gas_price = New TextBox()
+        label_number_of_passengers = New Label()
+        textBox_number_of_passengers = New TextBox()
+        label_driver_included = New Label()
+        radioBtn_yes = New RadioButton()
+        radioBtn_no = New RadioButton()
+        btn_calculate_fee = New Button()
+        btn_reset = New Button()
+        label_fee_per_person_tittle = New Label()
+        label_display_fee = New Label()
+        SuspendLayout()
+        ' 
+        ' label_title
+        ' 
+        label_title.AutoSize = True
+        label_title.Font = New Font("Modern No. 20", 35.9999962F, FontStyle.Bold, GraphicsUnit.Point)
+        label_title.Location = New Point(38, 18)
+        label_title.Name = "label_title"
+        label_title.Size = New Size(460, 50)
+        label_title.TabIndex = 0
+        label_title.Text = "Travel Fee Calculator"
+        ' 
+        ' label_number_of_kilometers
+        ' 
+        label_number_of_kilometers.AutoSize = True
+        label_number_of_kilometers.BackColor = Color.Black
+        label_number_of_kilometers.FlatStyle = FlatStyle.Flat
+        label_number_of_kilometers.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        label_number_of_kilometers.ForeColor = Color.White
+        label_number_of_kilometers.Location = New Point(38, 113)
+        label_number_of_kilometers.Name = "label_number_of_kilometers"
+        label_number_of_kilometers.Size = New Size(215, 26)
+        label_number_of_kilometers.TabIndex = 1
+        label_number_of_kilometers.Text = "Number of Kilometers"
+        ' 
+        ' textBox_num_of_km
+        ' 
+        textBox_num_of_km.BorderStyle = BorderStyle.FixedSingle
+        textBox_num_of_km.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        textBox_num_of_km.Location = New Point(322, 106)
+        textBox_num_of_km.Name = "textBox_num_of_km"
+        textBox_num_of_km.Size = New Size(100, 33)
+        textBox_num_of_km.TabIndex = 2
+        ' 
+        ' label_gas_consumption
+        ' 
+        label_gas_consumption.AutoSize = True
+        label_gas_consumption.BackColor = Color.Black
+        label_gas_consumption.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        label_gas_consumption.ForeColor = Color.White
+        label_gas_consumption.Location = New Point(38, 168)
+        label_gas_consumption.Name = "label_gas_consumption"
+        label_gas_consumption.Size = New Size(269, 26)
+        label_gas_consumption.TabIndex = 3
+        label_gas_consumption.Text = "Gas Consumption (L/100km)"
+        ' 
+        ' textBox_gas_consumption
+        ' 
+        textBox_gas_consumption.BorderStyle = BorderStyle.FixedSingle
+        textBox_gas_consumption.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        textBox_gas_consumption.Location = New Point(322, 161)
+        textBox_gas_consumption.Name = "textBox_gas_consumption"
+        textBox_gas_consumption.Size = New Size(100, 33)
+        textBox_gas_consumption.TabIndex = 4
+        ' 
+        ' label_gas_price
+        ' 
+        label_gas_price.AutoSize = True
+        label_gas_price.BackColor = Color.Black
+        label_gas_price.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        label_gas_price.ForeColor = Color.White
+        label_gas_price.Location = New Point(38, 224)
+        label_gas_price.Name = "label_gas_price"
+        label_gas_price.Size = New Size(139, 26)
+        label_gas_price.TabIndex = 5
+        label_gas_price.Text = "Gas Price ($/L)"
+        ' 
+        ' textBox_gas_price
+        ' 
+        textBox_gas_price.BorderStyle = BorderStyle.FixedSingle
+        textBox_gas_price.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        textBox_gas_price.Location = New Point(322, 217)
+        textBox_gas_price.Name = "textBox_gas_price"
+        textBox_gas_price.Size = New Size(100, 33)
+        textBox_gas_price.TabIndex = 6
+        ' 
+        ' label_number_of_passengers
+        ' 
+        label_number_of_passengers.AutoSize = True
+        label_number_of_passengers.BackColor = Color.Black
+        label_number_of_passengers.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        label_number_of_passengers.ForeColor = Color.White
+        label_number_of_passengers.Location = New Point(477, 109)
+        label_number_of_passengers.Name = "label_number_of_passengers"
+        label_number_of_passengers.Size = New Size(212, 26)
+        label_number_of_passengers.TabIndex = 7
+        label_number_of_passengers.Text = "Number of Passengers"
+        ' 
+        ' textBox_number_of_passengers
+        ' 
+        textBox_number_of_passengers.BorderStyle = BorderStyle.FixedSingle
+        textBox_number_of_passengers.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        textBox_number_of_passengers.Location = New Point(706, 102)
+        textBox_number_of_passengers.Name = "textBox_number_of_passengers"
+        textBox_number_of_passengers.Size = New Size(100, 33)
+        textBox_number_of_passengers.TabIndex = 8
+        ' 
+        ' label_driver_included
+        ' 
+        label_driver_included.AutoSize = True
+        label_driver_included.BackColor = Color.Black
+        label_driver_included.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        label_driver_included.ForeColor = Color.White
+        label_driver_included.Location = New Point(477, 168)
+        label_driver_included.Name = "label_driver_included"
+        label_driver_included.Size = New Size(163, 26)
+        label_driver_included.TabIndex = 9
+        label_driver_included.Text = "Driver Included?"
+        ' 
+        ' radioBtn_yes
+        ' 
+        radioBtn_yes.AutoSize = True
+        radioBtn_yes.BackColor = Color.Black
+        radioBtn_yes.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        radioBtn_yes.ForeColor = Color.Transparent
+        radioBtn_yes.Location = New Point(484, 210)
+        radioBtn_yes.Name = "radioBtn_yes"
+        radioBtn_yes.Padding = New Padding(5, 0, 0, 0)
+        radioBtn_yes.Size = New Size(64, 30)
+        radioBtn_yes.TabIndex = 10
+        radioBtn_yes.TabStop = True
+        radioBtn_yes.Text = "Yes"
+        radioBtn_yes.UseVisualStyleBackColor = False
+        ' 
+        ' radioBtn_no
+        ' 
+        radioBtn_no.AutoSize = True
+        radioBtn_no.BackColor = Color.Black
+        radioBtn_no.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        radioBtn_no.ForeColor = Color.Transparent
+        radioBtn_no.Location = New Point(581, 210)
+        radioBtn_no.Name = "radioBtn_no"
+        radioBtn_no.Padding = New Padding(5, 0, 0, 0)
+        radioBtn_no.Size = New Size(61, 30)
+        radioBtn_no.TabIndex = 11
+        radioBtn_no.TabStop = True
+        radioBtn_no.Text = "No"
+        radioBtn_no.UseVisualStyleBackColor = False
+        ' 
+        ' btn_calculate_fee
+        ' 
+        btn_calculate_fee.BackColor = Color.Black
+        btn_calculate_fee.Font = New Font("Palatino Linotype", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        btn_calculate_fee.ForeColor = Color.White
+        btn_calculate_fee.Location = New Point(38, 288)
+        btn_calculate_fee.Name = "btn_calculate_fee"
+        btn_calculate_fee.Size = New Size(406, 60)
+        btn_calculate_fee.TabIndex = 12
+        btn_calculate_fee.Text = "Calculate Fee"
+        btn_calculate_fee.UseVisualStyleBackColor = False
+        ' 
+        ' btn_reset
+        ' 
+        btn_reset.BackColor = Color.Black
+        btn_reset.Font = New Font("Palatino Linotype", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        btn_reset.ForeColor = Color.White
+        btn_reset.Location = New Point(38, 363)
+        btn_reset.Name = "btn_reset"
+        btn_reset.Size = New Size(406, 60)
+        btn_reset.TabIndex = 13
+        btn_reset.Text = "Reset"
+        btn_reset.UseVisualStyleBackColor = False
+        ' 
+        ' label_fee_per_person_tittle
+        ' 
+        label_fee_per_person_tittle.AutoSize = True
+        label_fee_per_person_tittle.BackColor = Color.Black
+        label_fee_per_person_tittle.Font = New Font("Palatino Linotype", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        label_fee_per_person_tittle.ForeColor = Color.White
+        label_fee_per_person_tittle.Location = New Point(484, 282)
+        label_fee_per_person_tittle.Name = "label_fee_per_person_tittle"
+        label_fee_per_person_tittle.Size = New Size(216, 37)
+        label_fee_per_person_tittle.TabIndex = 14
+        label_fee_per_person_tittle.Text = "Fee per person :"
+        ' 
+        ' label_display_fee
+        ' 
+        label_display_fee.AutoSize = True
+        label_display_fee.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(128))
+        label_display_fee.Font = New Font("Palatino Linotype", 36F, FontStyle.Bold, GraphicsUnit.Point)
+        label_display_fee.ForeColor = SystemColors.ActiveCaptionText
+        label_display_fee.Location = New Point(484, 332)
+        label_display_fee.Name = "label_display_fee"
+        label_display_fee.Size = New Size(0, 65)
+        label_display_fee.TabIndex = 17
+        ' 
+        ' TravelFeeCalculator
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(942, 450)
+        Controls.Add(label_display_fee)
+        Controls.Add(label_fee_per_person_tittle)
+        Controls.Add(btn_reset)
+        Controls.Add(btn_calculate_fee)
+        Controls.Add(radioBtn_no)
+        Controls.Add(radioBtn_yes)
+        Controls.Add(label_driver_included)
+        Controls.Add(textBox_number_of_passengers)
+        Controls.Add(label_number_of_passengers)
+        Controls.Add(textBox_gas_price)
+        Controls.Add(label_gas_price)
+        Controls.Add(textBox_gas_consumption)
+        Controls.Add(label_gas_consumption)
+        Controls.Add(textBox_num_of_km)
+        Controls.Add(label_number_of_kilometers)
+        Controls.Add(label_title)
+        FormBorderStyle = FormBorderStyle.FixedDialog
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "TravelFeeCalculator"
+        Text = "TravelFeeCalculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents label_title As Label
+    Friend WithEvents label_number_of_kilometers As Label
+    Friend WithEvents textBox_num_of_km As TextBox
+    Friend WithEvents label_gas_consumption As Label
+    Friend WithEvents textBox_gas_consumption As TextBox
+    Friend WithEvents label_gas_price As Label
+    Friend WithEvents textBox_gas_price As TextBox
+    Friend WithEvents label_number_of_passengers As Label
+    Friend WithEvents textBox_number_of_passengers As TextBox
+    Friend WithEvents label_driver_included As Label
+    Friend WithEvents radioBtn_yes As RadioButton
+    Friend WithEvents radioBtn_no As RadioButton
+    Friend WithEvents btn_calculate_fee As Button
+    Friend WithEvents btn_reset As Button
+    Friend WithEvents label_fee_per_person_tittle As Label
+    Friend WithEvents label_display_fee As Label
+End Class
---
+++ TravelFeeCalculator.vb
@@ -0,0 +1,212 @@
+﻿Imports System.Reflection.Emit
+Imports System.Security.Cryptography.X509Certificates
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Xml.Schema
+
+Public Class TravelFeeCalculator
+
+    'PUBLIC VARIABLES
+    'Input box variables
+    Public numOfKm, gasConsumption, gasPrice, numOfPassengers As String
+
+    'Converted strings
+    Public numOfPassengersInt As Integer
+    Public gasPriceDouble As Double
+    Public numOfKmInteger As Integer
+    Public gasConsumptionDouble As Double
+    Public feePerPerson As Double
+
+    Public driverIncluded, driverNotIncluded As Integer
+    Public totalPersons = 0
+
+    Private Sub btn_calculate_fee_Click(sender As Object, e As EventArgs) Handles btn_calculate_fee.Click
+        'Initialize isFormValid
+        Dim isFormValid As Boolean = True
+
+        'Initialize variables
+
+        'Set values for my variables
+        numOfKm = textBox_num_of_km.Text
+        gasConsumption = textBox_gas_consumption.Text
+        gasPrice = textBox_gas_price.Text
+        numOfPassengers = textBox_number_of_passengers.Text
+
+        'Initialize variables (driver included or not)
+
+
+
+
+        'Num of Km
+        If String.IsNullOrEmpty(numOfKm) Then
+            MsgBox("Number of kilometers cannot be empty", MsgBoxStyle.Exclamation, "Input")
+            isFormValid = False
+        Else
+            If Integer.TryParse(numOfKm, numOfKmInteger) Then
+                If numOfKmInteger < 1 OrElse numOfKmInteger > 10000 Then
+                    MsgBox("Number of kilometers must be between 1 and 10 000", MsgBoxStyle.Exclamation, "Input")
+                    isFormValid = False
+                End If
+            Else
+                MsgBox("Please enter a valid number of kilometers", MsgBoxStyle.Exclamation, "Input")
+                isFormValid = False
+            End If
+        End If
+
+        'Gas Consumption (between 1 and 60)
+        If String.IsNullOrEmpty(gasConsumption) Then
+            MsgBox("Gas consumption cannot be empty", MsgBoxStyle.Exclamation, "Input")
+            isFormValid = False
+        Else
+            If Double.TryParse(gasConsumption, gasConsumptionDouble) Then
+                If gasConsumptionDouble < 1 OrElse gasConsumptionDouble > 60 Then
+                    MsgBox("Gas consumption must be between 1 and 60", MsgBoxStyle.Exclamation, "Input")
+                    isFormValid = False
+                End If
+            Else
+                MsgBox("Please enter a valid number gas consumption", MsgBoxStyle.Exclamation, "Input")
+            End If
+        End If
+
+        'Gas Price (Between 0.5 and 3)
+        If String.IsNullOrEmpty(gasPrice) Then
+            MsgBox("Gas price cannot be empty", MsgBoxStyle.Exclamation, "Input")
+            isFormValid = False
+        Else
+            If Double.TryParse(gasPrice, gasPriceDouble) Then
+                If gasPriceDouble < 0.5 OrElse gasPriceDouble > 3 Then
+                    MsgBox("Gas price must be between 0.50$ ans 4.00$", MsgBoxStyle.Exclamation, "Input")
+                    isFormValid = False
+                End If
+            Else
+                MsgBox("Please enter a valid gas price", MsgBoxStyle.Exclamation, "Input")
+            End If
+        End If
+
+        'Number of Passengers
+        If String.IsNullOrEmpty(numOfPassengers) Then
+            MsgBox("Number of passengers cannot be empty", MsgBoxStyle.Exclamation, "Input")
+            isFormValid = False
+        Else
+            If Integer.TryParse(numOfPassengers, numOfPassengersInt) Then
+                If numOfPassengersInt < 1 OrElse numOfPassengersInt > 10 Then
+                    MsgBox("Number of passengers must be between 1 and 10", MsgBoxStyle.Exclamation, "Input")
+                    isFormValid = False
+                End If
+            Else
+                MsgBox("Please enter a valid number of passengers", MsgBoxStyle.Exclamation, "Input")
+                isFormValid = False
+            End If
+        End If
+
+
+        'Select Include/Not Include drive
+        If radioBtn_yes.Checked = False And radioBtn_no.Checked = False Then
+            MsgBox("Is the drive included? Please select Yes or No", MsgBoxStyle.Exclamation, "Input")
+            isFormValid = False
+        Else
+            If radioBtn_yes.Checked = True Then
+                totalPersons = numOfPassengersInt + 0
+            Else
+                totalPersons = numOfPassengersInt + 1
+            End If
+        End If
+
+        'CALCULATE AND DISPLAY
+        If isFormValid Then
+            feePerPerson = getFeePerPerson(numOfKmInteger, gasConsumptionDouble, gasPriceDouble, totalPersons)
+            'F2 = Format 2 decimals
+            label_display_fee.Text = feePerPerson.ToString("F2") & "$"
+        End If
+
+    End Sub
+    'FUNCTIONS
+    Public Function getFeePerPerson(ByRef kmToTravel As Double, ByRef consumption As Double, ByRef gasPrice As Double, ByRef numberOfPassengers As Integer) As Double
+        ' Calculate the total cost of gas for the trip
+        Dim totalGasCost As Double = kmToTravel * (consumption / 100.0) * gasPrice
+
+        ' Calculate the fee per person
+        Dim feePerPerson As Double
+
+        feePerPerson = totalGasCost / numberOfPassengers
+
+        ' Return the fee per person
+        Return feePerPerson
+    End Function
+
+
+    'SUBS
+    'Reset the input boxes
+    Sub resetInputBoxes()
+        textBox_num_of_km.Text = ""
+        textBox_gas_consumption.Text = ""
+        textBox_gas_price.Text = ""
+        textBox_number_of_passengers.Text = ""
+        label_display_fee.Text = ""
+        radioBtn_yes.Checked = False
+        radioBtn_no.Checked = False
+    End Sub
+
+
+    'Gas consumption (2 digits, 2 decimals)
+    Private Sub textBox_gas_consumption_KeyPress(sender As Object, e As KeyPressEventArgs) Handles textBox_gas_consumption.KeyPress
+        ' Allow numbers, the backspace key, and the decimal point
+        ' IsControl checks for control characters like backspace
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) AndAlso e.KeyChar <> "."c Then
+            e.Handled = True ' Reject the character
+        End If
+
+        ' Only allow one decimal point
+        If e.KeyChar = "."c AndAlso textBox_gas_consumption.Text.IndexOf("."c) > -1 Then
+            e.Handled = True ' Reject the character
+        End If
+
+        ' Check if the text already has a decimal point and two numbers after it
+        'Splitting the number in 2 arrays (after the ".")
+        ' "."c means I am refering to the character for the period or decimal
+        If textBox_gas_consumption.Text.Contains(".") AndAlso textBox_gas_consumption.Text.Split("."c)(1).Length >= 2 AndAlso Char.IsDigit(e.KeyChar) AndAlso textBox_gas_consumption.SelectionStart > textBox_gas_consumption.Text.IndexOf("."c) Then
+            e.Handled = True ' Reject the character
+        End If
+
+        ' Check if the text is at the maximum length before the decimal
+        If Not textBox_gas_consumption.Text.Contains(".") AndAlso textBox_gas_consumption.Text.Length >= 2 AndAlso Char.IsDigit(e.KeyChar) Then
+            If textBox_gas_consumption.SelectionStart <= 1 Then
+                ' Allow typing in the first two positions
+                e.Handled = False
+            Else
+                ' If the cursor is beyond the first two digits, reject the character
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    'Gas price (1 digit, 2 decimals)
+    Private Sub textBox_gas_price_KeyPress(sender As Object, e As KeyPressEventArgs) Handles textBox_gas_price.KeyPress
+        ' Allow numbers, the backspace key, and the decimal point
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) AndAlso e.KeyChar <> "."c Then
+            e.Handled = True ' Reject the character
+        End If
+
+        ' Only allow one decimal point
+        If e.KeyChar = "."c AndAlso textBox_gas_price.Text.IndexOf("."c) > -1 Then
+            e.Handled = True ' Reject the character
+        End If
+
+        ' Check if the text already has a decimal point and two numbers after it
+        If textBox_gas_price.Text.Contains(".") AndAlso textBox_gas_price.Text.Split("."c)(1).Length >= 2 AndAlso Char.IsDigit(e.KeyChar) AndAlso textBox_gas_price.SelectionStart > textBox_gas_price.Text.IndexOf("."c) Then
+            e.Handled = True ' Reject the character
+        End If
+
+        ' Check if the text is at the maximum length before the decimal
+        If Not textBox_gas_price.Text.Contains(".") AndAlso textBox_gas_price.Text.Length >= 1 AndAlso Char.IsDigit(e.KeyChar) Then
+            ' If the cursor is beyond the first digit, reject the character unless it's the decimal point
+            If textBox_gas_price.SelectionStart > 1 OrElse (textBox_gas_price.SelectionStart = 1 AndAlso e.KeyChar <> "."c) Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_reset_Click(sender As Object, e As EventArgs) Handles btn_reset.Click
+        resetInputBoxes()
+    End Sub
+End Class
